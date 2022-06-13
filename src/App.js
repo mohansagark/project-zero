8369d5b206bff02f107c7cf2e29c72e15f981e00
Divya Sri Manda <divyasri.manda@outlook.com>
2022-06-13 12:57:34 +0530

    added header and back nvigation

--- src/App.js
+++ src/App.js
@@ -3,19 +3,28 @@ import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <h1>Project Zero</h1>
-      <nav
-        style={{
-          borderBottom: "solid 1px",
-          paddingBottom: "1rem",
-        }}
-      >
-        <Link to="todoApp">ToDo App</Link> |{" "}
-        <Link to="expenseTracker">Expense Tracker</Link> |{" "}
-        <Link to="passwordGenerator">Password Generator</Link> |{" "}
-        <Link to="about">About</Link>
-      </nav>
+    <div className="container-fluid nav">
+      <div className="row margin">
+        <div className="col-md-3 links">
+          <span className="project-title">Project Zero</span>
+        </div>
+        <div className="col-md-8 links">
+          <Link to="todoApp" className="text-white">
+            ToDo App
+          </Link>{" "}
+          <Link to="expenseTracker" className="text-white">
+            Expense Tracker
+          </Link>{" "}
+          <Link to="passwordGenerator" className="text-white">
+            Password Generator
+          </Link>{" "}
+          <Link to="about">About</Link>
+        </div>
+        {/* <div className="col-md-2"></div>
+        <div className="col-md-2"></div>
+        <div className="col-md-2"></div> */}
+        <div className="col-md-1"></div>
+      </div>
     </div>
   );
 }
